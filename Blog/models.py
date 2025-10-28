c24f27e175450bf1deded6a2c70aa1484581894d
nima11ak <nimaak98@yahoo.com>
2025-10-28 15:41:32 +0330

    database update

--- Blog/models.py
+++ Blog/models.py
@@ -3,3 +3,15 @@ from django.db import models
 class post(models.Model):
     title = models.CharField(max_length=100)
     content =models.TextField()
+
+
+    #tag
+    #category
+
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=0)
+    created_date = models.DateTimeField(null=True)
+    published_date = models. DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+

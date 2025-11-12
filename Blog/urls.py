df82ddf8ebcc523b57abfa5827bb53d050ed27bc
nima11ak <nimaak98@yahoo.com>
2025-11-12 15:00:43 +0330

    fix the bug

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
-from Blog.views import *
-
+from . import views
 from Blog.views import blog_single, blog_view
 
 app_name ="Blog"
 
 urlpatterns = [
-    path('',blog_view,name="index"),
-    path('test',test,name="test"),
-    
-    
+    path('',views.blog_view, name="index"),
+    path('post-<int:pid>/', views.test, name='test'),
+    path('post-<str:pid>/', views.blog_single, name='blog_single'),
 
 ]
+ 
--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from Blog.models import Post
+
 
 def blog_view(request):
     posts = Post.objects.all()  # <-- این خط اضافه شود
@@ -7,11 +8,13 @@ def blog_view(request):
     return render(request, 'Blog/blog.html', context)
 
 
-def blog_single(request):
-    return render(request,'Blog/blog-single.html')
+def blog_single(request, pid):
+    post = get_object_or_404(Post, id=pid)
+    context = {'post': post, 'single': True}  
+    return render(request, 'Blog/blog-single.html', context)
 
 
-def test(request):
-    posts = Post.objects.all()
-    context = {'posts':posts}
-    return render(request,'test.html',context)
+def test(request, pid):
+    post = get_object_or_404(Post, id=pid)
+    context = {'post': post}
+    return render(request, 'test.html', context)
